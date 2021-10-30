7087fe647e1287f954fec25a626b212079c194fb
Clifton McCook <cliftonmccook@gmail.com>
2021-10-29 19:57:32 -0500

    Edited app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 window.onload = () => {
+    document.getElementsByTagName("input")[0].value = '';
     let button = document.getElementById("search-button");
     button.addEventListener("click", getSuperHeroes);
 };
 
-// Display list of superheores in alert
+/*
+// Exercise 2 - Display list of superheores in alert
 function getSuperHeroes(event) {
     event.preventDefault();
     let url = "superheroes.php";
@@ -16,5 +18,24 @@ function getSuperHeroes(event) {
         }
     })
     .then(data => {alert(data);})
+    .catch(error => alert("There was an error: " + error));
+}*/
+
+// Exercise 3 - Search by name or alias
+function getSuperHeroes(event) {
+    event.preventDefault();
+    let key = document.getElementsByTagName("input")[0].name;
+    let value = document.getElementsByTagName("input")[0].value;
+    let params = '?' + `${key}=${value}`;
+    let url = "superheroes.php"+params;
+    fetch(url)
+    .then(response => {
+        if (response.ok) {
+            return response.text();
+        } else {
+            return Promise.reject("Promise rejected");
+        }
+    })
+    .then(data => {document.getElementById("result").innerHTML = data;})
     .catch(error => alert("There was an error: " + error));
 }
